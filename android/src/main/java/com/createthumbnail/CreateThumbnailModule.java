57186336a248c6d28debbb133b0751ea9c470f5a
Souvik Ghosh <emailtosvk@gmail.com>
2019-12-21 00:03:29 +0530

    cache android

--- android/src/main/java/com/createthumbnail/CreateThumbnailModule.java
+++ android/src/main/java/com/createthumbnail/CreateThumbnailModule.java
@@ -5,16 +5,12 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 import java.util.HashMap;
+import java.util.Arrays;
 
 import android.graphics.Bitmap;
-import android.graphics.Matrix;
 import android.media.MediaMetadataRetriever;
-import android.media.ThumbnailUtils;
 import android.os.Build.VERSION;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.provider.MediaStore.Video.Thumbnails;
-import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -24,9 +20,12 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+
 public class CreateThumbnailModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private static final long CACHE_DIR_MAX_SIZE = 52428800L; // 50MB
 
     public CreateThumbnailModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -45,7 +44,7 @@ public class CreateThumbnailModule extends ReactContextBaseJavaModule {
         String format = options.hasKey("format") ? options.getString("format") : "jpeg";
         int timeStamp = options.hasKey("timeStamp") ? options.getInt("timeStamp") : 1;
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-        String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/thumb";
+        String thumbnailDir = reactContext.getApplicationContext().getCacheDir().getAbsolutePath() + "/thumbnails";
         String fileName = "thumb-" + UUID.randomUUID().toString() + "." + format;
         OutputStream fOut = null;
 
@@ -63,27 +62,34 @@ public class CreateThumbnailModule extends ReactContextBaseJavaModule {
             Bitmap image = retriever.getFrameAtTime(timeStamp * 1000000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
             retriever.release();
 
-            File dir = new File(fullPath);
+            File dir = new File(thumbnailDir);
             if (!dir.exists()) {
                 dir.mkdirs();
             }
 
-            File file = new File(fullPath, fileName);
+            File file = new File(thumbnailDir, fileName);
             file.createNewFile();
+
             fOut = new FileOutputStream(file);
 
             // 100 means no compression, the lower you go, the stronger the compression
             if (format == "png") {
                 image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
             } else {
-                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+                image.compress(Bitmap.CompressFormat.JPEG, 90, fOut);
             }
 
             fOut.flush();
             fOut.close();
 
+            long newSize = image.getByteCount() + getDirSize(dir);
+            // free up some cached data if size of cache dir exceeds CACHE_DIR_MAX_SIZE
+            if (newSize > CACHE_DIR_MAX_SIZE) {
+                cleanDir(dir, CACHE_DIR_MAX_SIZE / 2);
+            }
+
             WritableMap map = Arguments.createMap();
-            map.putString("path", "file://" + fullPath + '/' + fileName);
+            map.putString("path", "file://" + thumbnailDir + '/' + fileName);
             map.putDouble("width", image.getWidth());
             map.putDouble("height", image.getHeight());
 
@@ -91,5 +97,34 @@ public class CreateThumbnailModule extends ReactContextBaseJavaModule {
         } catch (Exception e) {
             promise.reject("CreateThumbnail_ERROR", e);
         }
+    }
+
+    // delete previously added files one by one untill requred space is available
+    private static void cleanDir(File dir, long bytes) {
+        long bytesDeleted = 0;
+        File[] files = dir.listFiles();
+        Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_COMPARATOR);
+
+        for (File file : files) {
+            bytesDeleted += file.length();
+            file.delete();
+
+            if (bytesDeleted >= bytes) {
+                break;
+            }
+        }
+    }
+
+    private static long getDirSize(File dir) {
+        long size = 0;
+        File[] files = dir.listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                size += file.length();
+            }
+        }
+
+        return size;
     }
 }
